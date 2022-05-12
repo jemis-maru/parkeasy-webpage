e8d5c548623c49ca15c626df339d07541abd872c
jemis-maru <jemis.maru@bacancy.com>
2022-05-12 17:56:11 +0530

    Complete a webpage design.

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+const slider = document.getElementById('scrollbar-container');
+let mouseDown = false;
+let startX, scrollLeft;
+
+let startDragging = function (e) {
+    mouseDown = true;
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+};
+let stopDragging = function (event) {
+    mouseDown = false;
+};
+
+slider.addEventListener('mousemove', (e) => {
+    e.preventDefault();
+    if(!mouseDown) { return; }
+    const x = e.pageX - slider.offsetLeft;
+    const scroll = x - startX;
+    slider.scrollLeft = scrollLeft - scroll;
+});
+
+slider.addEventListener('mousedown', startDragging, false);
+slider.addEventListener('mouseup', stopDragging, false);
+slider.addEventListener('mouseleave', stopDragging, false);
+
+
+
 let buttonStatus = 'play';
 
 function toggleDrawer() {
@@ -49,4 +76,21 @@ function hoverOut(){
     playBtn.classList.add("play-btn-toggle");
     let pauseBtn = document.getElementById("pause-btn");
     pauseBtn.classList.add("play-btn-toggle");
+}
+
+
+function myMap() {
+    var mapProp= {
+        center:new google.maps.LatLng(23.04974206415296, 72.50157402091503),
+        zoom: 10,
+    };
+    var map = new google.maps.Map(document.getElementById("google-map"), mapProp);
+    map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
+    var marker = new google.maps.Marker({
+        position: {
+            lat: 23.04974206415296,
+            lng: 72.50157402091503
+        },
+        map: map,
+    });
 }
